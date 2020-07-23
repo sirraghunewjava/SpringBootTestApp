7cd9e5191ffe2e72c0514a5bfe0e65fccc3c5d9a
User <User@192.168.0.7>
2020-07-23 10:01:22 +0530

    WH-8520 - First Time File Update

---
+++ src/main/java/in/nareshit/raghu/runner/MessageRunner.java
@@ -0,0 +1,14 @@
+package in.nareshit.raghu.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("WELCOME TO SPRING BOOT APP!");
+	}
+
+}
